5522066c081c2fea0749aca79038f11f2864de8c
Youssef Zidan <yz12345678900987654321@gmail.com>
2026-04-03 14:40:39 +0200

    implement: Zen Browser App

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import dayjs from "dayjs";
 
 const Navbar = () => {
     return (
-        <nav>
-            <div>
-                <img src="/images/logo.svg" alt="logo" />
-                <p className="font-bold">Yousef's Portfolio</p>
-                <ul>
+        <nav className="relative z-50">
+            <div className="flex items-center gap-4">
+                <img src="/images/logo.svg" alt="logo" className="w-5 h-5"/>
+                <p className="font-bold whitespace-nowrap">Yousef's Portfolio</p>
+                <ul className="flex items-center gap-3">
                     {navLinks.map(({ id, name }) => (
-                        <li key={id}>
-                            <p>{name}</p>
+                        <li key={id} className="relative">
+                            <p className="z-10 p-1 rounded text-sm cursor-pointer hover:bg-blue-200 hover:text-blue-900 transition-all">{name}</p>
                         </li>
                     ))
                     }
